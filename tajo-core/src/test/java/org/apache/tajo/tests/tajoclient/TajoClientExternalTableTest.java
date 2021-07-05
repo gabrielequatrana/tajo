167301de8da4ddb95f83d2ccf62a8f8c8563ab33
Gabriele <gabriele.quatrana@outlook.com>
2021-07-05 20:03:18 +0200

    Test #8

--- tajo-core/src/test/java/org/apache/tajo/tests/tajoclient/TajoClientExternalTableTest.java
+++ tajo-core/src/test/java/org/apache/tajo/tests/tajoclient/TajoClientExternalTableTest.java
@@ -1,4 +1,4 @@
-package org.apache.tajo.tests;
+package org.apache.tajo.tests.tajoclient;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -7,15 +7,22 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.tajo.client.TajoClient;
+import org.apache.tajo.conf.TajoConf;
 import org.apache.tajo.exception.CannotDropCurrentDatabaseException;
+import org.apache.tajo.exception.DuplicateTableException;
 import org.apache.tajo.exception.InsufficientPrivilegeException;
 import org.apache.tajo.exception.SQLSyntaxError;
 import org.apache.tajo.exception.TajoException;
+import org.apache.tajo.exception.UnavailableTableLocationException;
 import org.apache.tajo.exception.UndefinedDatabaseException;
 import org.apache.tajo.exception.UndefinedTableException;
+import org.apache.tajo.storage.StorageUtil;
+import org.apache.tajo.tests.util.BackendTestingUtil;
 import org.apache.tajo.tests.util.TajoTestingCluster;
 import org.apache.tajo.tests.util.TpchTestBase;
+import org.apache.tajo.util.CommonTestingUtil;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -27,37 +34,44 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class TajoClientTableTest {
-	
+public class TajoClientExternalTableTest {
+
 	// TajoClient instance
 	private static TajoClient client;
-	
+
 	// Test parameters
 	private String tableName;
+	private Path path;
 	private Class<? extends Exception> expectedException;
-	
+
 	// Test environment
 	private static TajoTestingCluster cluster;
-	
+	private static TajoConf conf;
+	private static Path testDir;
+
 	@Rule
 	public ExpectedException exceptionRule = ExpectedException.none();
 
-	public TajoClientTableTest(String tableName, Class<? extends Exception> expectedException) {
+	public TajoClientExternalTableTest(String tableName, String path, Class<? extends Exception> expectedException) throws IOException {
 		this.tableName = tableName;
+		this.path = createTempTable(path);
 		this.expectedException = expectedException;
 	}
 
 	@Parameters
-	public static Collection<Object[]> getParameters() {
-		return Arrays.asList(new Object[][] {
-			{ "test_table", null }, 
-			{ "", SQLSyntaxError.class }
+	public static Collection<Object[]> getParameters() throws IOException {
+		return Arrays.asList(new Object[][] { 
+			{ "test_table", "test_table", null }, 
+			{ "", "", SQLSyntaxError.class },
+			{ "", null, SQLSyntaxError.class }
 		});
 	}
 
 	@BeforeClass
 	public static void setUp() throws Exception {
 		cluster = TpchTestBase.getInstance().getTestingCluster();
+		conf = cluster.getConfiguration();
+		testDir = CommonTestingUtil.getTestDir();
 		client = cluster.newTajoClient();
 	}
 
@@ -75,84 +89,112 @@ public class TajoClientTableTest {
 			}
 		}
 	}
-	
+
 	@Test
-	public void createAndDropTableByQueryTest() throws TajoException, IOException {
-		System.out.println("\n*************** TEST ***************");	
-		
+	public void createAndDropExternalTableTest() throws UndefinedTableException, InsufficientPrivilegeException, DuplicateTableException, UnavailableTableLocationException, IOException {
+		System.out.println("\n*************** TEST ***************");
+
+		int after = client.getTableList(null).size();
+
+		client.createExternalTable(tableName, BackendTestingUtil.mockupSchema, path.toUri(), BackendTestingUtil.mockupMeta);
+
+		System.out.println("\n-------------- CREATE --------------");
+		System.out.println("Created table: " + tableName);
+		System.out.println("N. of tables: " + client.getTableList(null).size());
+
+		assertTrue(client.existTable(tableName));
+
+		client.dropTable(tableName);
+
+		int before = client.getTableList(null).size();
+
+		System.out.println("\n-------------- DROP --------------");
+		System.out.println("Dropped table: " + tableName);
+		System.out.println("N. of tables: " + before);
+
+		System.out.println("\n-------------- RESULT --------------");
+		System.out.println("After: " + after);
+		System.out.println("Before: " + before);
+
+		assertFalse(client.existTable(tableName));
+
+		System.out.println("\n************************************\n");
+	}
+
+	@Test
+	public void createAndDropExternalTableByQueryTest() throws TajoException, IOException {
+		System.out.println("\n*************** TEST ***************");
+
 		if (expectedException != null) {
 			exceptionRule.expect(expectedException);
 			System.out.println("Raised exception: " + expectedException.getName());
 		}
-		
+
 		int after = client.getTableList(null).size();
 
-		String sql1 = "create table " + tableName + " (deptname text, score int4)";
-		
-		client.updateQuery(sql1);
-		
+		String sql1 = "create external table " + tableName + " (deptname text, score int4) " + "using csv location '" + path + "'";
+
+		client.executeQueryAndGetResult(sql1);
+
 		System.out.println("\n-------------- CREATE --------------");
 		System.out.println("Created table: " + tableName);
 		System.out.println("N. of tables: " + client.getTableList(null).size());
-		
+
 		assertTrue(client.existTable(tableName));
 
 		String sql2 = "drop table " + tableName;
-		
+
 		client.updateQuery(sql2);
-		
+
 		int before = client.getTableList(null).size();
-		
+
 		System.out.println("\n-------------- DROP --------------");
 		System.out.println("Dropped table: " + tableName);
 		System.out.println("N. of tables: " + before);
-	
+
 		System.out.println("\n-------------- RESULT --------------");
 		System.out.println("After: " + after);
 		System.out.println("Before: " + before);
-		
+
 		assertFalse(client.existTable(tableName));
-		
+
 		System.out.println("\n************************************\n");
 	}
-	
-	@Test
-	public void createAndPurgeTableByQueryTest() throws TajoException, IOException {
-		System.out.println("\n*************** TEST ***************");	
-		
-		if (expectedException != null) {
-			exceptionRule.expect(expectedException);
-			System.out.println("Raised exception: " + expectedException.getName());
-		}
-		
-		int after = client.getTableList(null).size();
 
-		String sql1 = "create table " + tableName + " (deptname text, score int4)";
+	@Test
+	public void createAndPurgeExternalTableTest() throws UndefinedTableException, InsufficientPrivilegeException, DuplicateTableException, UnavailableTableLocationException, IOException {
+		System.out.println("\n*************** TEST ***************");
+
+		int after = client.getTableList(null).size();
 		
-		client.updateQuery(sql1);
-		
+		client.createExternalTable(tableName, BackendTestingUtil.mockupSchema, path.toUri(), BackendTestingUtil.mockupMeta);
+
 		System.out.println("\n-------------- CREATE --------------");
 		System.out.println("Created table: " + tableName);
 		System.out.println("N. of tables: " + client.getTableList(null).size());
-		
+
 		assertTrue(client.existTable(tableName));
 
-		String sql2 = "drop table " + tableName + " purge";
-		
-		client.updateQuery(sql2);
-		
+		client.dropTable(tableName, true);
+
 		int before = client.getTableList(null).size();
-		
+
 		System.out.println("\n-------------- DROP --------------");
 		System.out.println("Dropped table: " + tableName);
 		System.out.println("N. of tables: " + before);
-	
+
 		System.out.println("\n-------------- RESULT --------------");
 		System.out.println("After: " + after);
 		System.out.println("Before: " + before);
-		
+
 		assertFalse(client.existTable(tableName));
-		
+
 		System.out.println("\n************************************\n");
+	}
+
+	private Path createTempTable(String tableName) throws IOException {
+		Path tablePath = StorageUtil.concatPath(testDir, tableName);
+		BackendTestingUtil.writeTmpTable(conf, tablePath);
+		return tablePath;
 	}
 }
--- tajo-core/src/test/java/org/apache/tajo/tests/tajoclient/TajoClientSessionVariableTest.java
+++ tajo-core/src/test/java/org/apache/tajo/tests/tajoclient/TajoClientSessionVariableTest.java
@@ -1,4 +1,4 @@
-package org.apache.tajo.tests;
+package org.apache.tajo.tests.tajoclient;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
